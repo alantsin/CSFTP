63e0a1cffc74e8faac69e8d2701a4aad87e8e194
Alan <alantsin92@gmail.com>
2016-11-28 02:54:07 -0800

    USER and Quit implemented

--- CSftp.c
+++ CSftp.c
@@ -1,132 +1,143 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
+#include <string.h>    
+#include <stdlib.h>    
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
+#include <unistd.h>  
+#include <pthread.h> 
 #include "dir.h"
 #include "usage.h"
 
-#define PORT "3490"  // the port users will be connecting to
+#define PORT 8888
 
-#define BACKLOG 10     // how many pending connections queue will hold
+#define BACKLOG 10
 
-void sigchld_handler(int s)
-{
-    // waitpid() might overwrite errno, so we save and restore it:
-    int saved_errno = errno;
-
-    while(waitpid(-1, NULL, WNOHANG) > 0);
-
-    errno = saved_errno;
-}
-
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
+//the thread function prototype
+void *connection_handler(void *);
 
 int main(int argc, char **argv) {
 
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes=1;
-    char s[INET6_ADDRSTRLEN];
-    int rv;
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    int server_sock , client_sock , c , *new_sock;
+	
+    struct sockaddr_in server , client;
+     
+    //Create socket
+    server_sock = socket(AF_INET , SOCK_STREAM , 0);
+	
+    if (server_sock == -1) {
+        perror("Failed to create socket. Error.\n");
+		return 1;
+    }
+	
+    puts("Socket created.");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( PORT );
+     
+    //Bind
+    if( bind(server_sock,(struct sockaddr *)&server , sizeof(server)) < 0) {
+        perror("Bind failed. Error.");
         return 1;
     }
-
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("server: socket");
-            continue;
+	
+    puts("Bind done.");
+     
+    //Listen
+    listen(server_sock , BACKLOG);
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+     
+    while( (client_sock = accept(server_sock, (struct sockaddr *)&client, (socklen_t*)&c)) ) {
+        puts("Connection accepted.");
+         
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = client_sock;
+         
+        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0) {
+            perror("Failed to create thread. Error.");
+            return 1;
         }
-
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
-            perror("setsockopt");
-            exit(1);
-        }
-
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
-
-        break;
+         
+      //  pthread_join( sniffer_thread , NULL);
+		
+        puts("Closing connection.");
     }
-
-    freeaddrinfo(servinfo); // all done with this structure
-
-    if (p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
-        exit(1);
+     
+    if (client_sock < 0)
+    {
+        perror("Accept failed. Error.");
+        return 1;
     }
-
-    if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
-        exit(1);
-    }
-
-    sa.sa_handler = sigchld_handler; // reap all dead processes
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
-
-    printf("server: waiting for connections...\n");
-
-    while(1) {  // main accept() loop
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
-            perror("accept");
-            continue;
-        }
-
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
-        printf("server: got connection from %s\n", s);
-
-        if (!fork()) { // this is the child process
-            close(sockfd); // child doesn't need the listener
-            if (send(new_fd, "Hello, world!\n", 13, 0) == -1)
-                perror("send");
-            close(new_fd);
-            exit(0);
-        }
-        close(new_fd);  // parent doesn't need this
-    }
-
+     
     return 0;
 
+}
+
+void *connection_handler(void *server_sock) {
+
+    int sock = *(int*)server_sock;
+    int read_size;
+    char *message , client_message[2000], command[5], user[5];
+	int logged_in = 0;
+     
+    message = "<-- Welcome to CPSC 317 Assignment 3 FTP server by Alan Tsin.\n";
+    write(sock , message , strlen(message));
+     
+    message = "<-- 220 To begin, specify your username. This server only supports the username: cs317\n";
+    write(sock , message , strlen(message));
+     
+    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 ) {
+		
+		sscanf(client_message, "%s", command);
+		
+		if (!strcasecmp(command, "user")) {
+			// If not already logged in
+			if (logged_in == 0) {
+				// Compare provided username with "cs317"
+				sscanf(client_message, "%s%s", user, user);
+				if (!strcasecmp(user, "cs317")) {
+					logged_in = 1;
+					message = "<-- 230 Login successful.\n";
+					write(sock , message , strlen(message));
+				}
+				// Does not support any other username
+				else {
+					message = "<-- 530 This server only supports the username: cs317\n";
+					write(sock , message , strlen(message));
+				}
+			}
+			// If already logged in
+			else {
+				message = "<-- 530 Can't change from cs317 user.\n";
+				write(sock , message , strlen(message));
+			}
+			
+		}
+		
+		
+		else if (!strcmp(command, "quit")) {
+			message = "User has quit\n";
+			write(sock , message , strlen(message));
+			fflush(stdout);
+			break;
+		}
+		
+		else {
+		}
+
+    }
+     
+	if(read_size == -1) {
+        perror("recv failed");
+    }
+         
+    //Free the socket pointer
+    free(server_sock);
+     
+    return 0;
 }
